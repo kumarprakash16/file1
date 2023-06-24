f61199b524b37f13e7d5aad0bc0de068cde6274f
kumarprakash16 <kumarprakash1636@gmail.com>
2023-06-24 20:06:48 +0530

    Akki is here

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x=1;
+    int x=2;
+    if(true)
+    {
+        int x=2;
+    }
+    cout<<x;
+    cout<<"Akki the boss";
+    return 0;
+}
